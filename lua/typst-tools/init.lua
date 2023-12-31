4c0a69b902425fca8e6ed7a8d74debd367fa28cd
Max <max.heinzer397@gmx.ch>
2023-12-31 11:18:48 +0100

    feat: add functionality for toc and heading jumping

--- lua/typst-tools/init.lua
+++ lua/typst-tools/init.lua
@@ -3,14 +3,35 @@ local typst = {}
 typst.config = {
     treesitter = true,
     lsp = {
-        enabled = true,
+        enabled = false,
         on_attach = function() end,
     },
     formatter = {
         formatter_nvim = false,
         conform_nvim = false,
     },
+    default_mappings = true,
 }
+
+local function create_commands()
+    vim.api.nvim_create_user_command("TypstToc", function()
+        require("typst-tools.utils").heading_loclist()
+    end, {})
+end
+
+local function create_default_mappings()
+    vim.api.nvim_create_autocmd("BufEnter", {
+        pattern = "*.typ",
+        callback = function()
+            vim.keymap.set("n", "]]", function()
+                require("typst-tools.utils").jump_to_next_heading()
+            end, { buffer = true })
+            vim.keymap.set("n", "[[", function()
+                require("typst-tools.utils").jump_to_previous_heading()
+            end, { buffer = true })
+        end,
+    })
+end
 
 function typst.setup(opts)
     vim.filetype.add({
@@ -28,6 +49,12 @@ function typst.setup(opts)
 
     if typst.config.lsp.enabled then
         require("typst-tools.lsp").setup(typst.config.lsp)
+    end
+
+    create_commands()
+
+    if typst.config.default_mappings then
+        create_default_mappings()
     end
 
     vim.api.nvim_create_autocmd("FileType", {
